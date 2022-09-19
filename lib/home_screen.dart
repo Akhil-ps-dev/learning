df59280ef034bc2a96f6cd90180d2e986379fe15
Chatter-Boo <psakhil211@gmail.com>
2022-09-19 22:36:20 +0530

    dropdown cmptld

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 
 class MYhome extends StatelessWidget {
-  const MYhome({super.key});
+  final _list = [
+    'apple',
+    'banana',
+    'orange',
+    'grape',
+    'watermelon',
+    'mango',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('main'),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DropdownButton(
+              items: _list.map((e) {
+                return DropdownMenuItem(
+                  value: e,
+                  child: Text(e),
+                );
+              }).toList(),
+              onChanged: (value) {
+                print(value);
+              })
+        ],
+      )),
     );
   }
 }
